2de99257ef7f3f32d4eb7df2c77f485bdbecc0dc
Cuong Giang <thaicuong.giang@gmail.com>
2024-09-01 20:52:20 +0700

    tweak iterm2 config

--- install/desktop/app-iterm2.sh
+++ install/desktop/app-iterm2.sh
@@ -3,5 +3,8 @@ if [ ! -d /Applications/iTerm.app ]; then
 fi
 
 cp $HOME/.local/share/omakma/configs/iterm2.json $HOME/Library/Application\ Support/iTerm2/DynamicProfiles/omakma.json
-defaults write com.googlecode.iterm2.plist "Default Bookmark Guid" -string 5D7D59C7-B2AA-40B4-ACE7-3052E6456A50
+ITERM2_DEFAULT_PROFILE_GUID=$(uuidgen)
+gsed -i "s/\"Guid\" : \".*\"/\"Guid\" : \"$ITERM2_DEFAULT_PROFILE_GUID\"/g" $HOME/Library/Application\ Support/iTerm2/DynamicProfiles/omakma.json
+defaults write com.googlecode.iterm2.plist "Default Bookmark Guid" -string $ITERM2_DEFAULT_PROFILE_GUID
 plutil -convert xml1 $HOME/Library/Preferences/com.googlecode.iterm2.plist
+# mkdir -p $HOME/Library/Application\ Support/iTerm2/Scripts/AutoLaunch
