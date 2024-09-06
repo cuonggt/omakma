873d1c83d978f3e6164fbd3b113bc2fdc5fe771d
Cuong Giang <thaicuong.giang@gmail.com>
2024-09-06 14:51:40 +0700

    update first run choices

--- install/first-run-choices.sh
+++ install/first-run-choices.sh
@@ -1,7 +1,7 @@
-OPTIONAL_APPS=("Zed" "Dropbox" "Pritunl" "Xmind" "Brave")
-DEFAULT_OPTIONAL_APPS="Zed","Dropbox","Pritunl","Xmind"
+OPTIONAL_APPS=("1111" "Dropbox" "Pritunl" "Xmind" "Zed")
+DEFAULT_OPTIONAL_APPS="1111","Dropbox","Pritunl","Xmind","Zed"
 export OMAKMA_FIRST_RUN_OPTIONAL_APPS=$(gum choose "${OPTIONAL_APPS[@]}" --no-limit --selected $DEFAULT_OPTIONAL_APPS --height 7 --header "Select optional apps" | tr ' ' '-')
 
 AVAILABLE_LANGUAGES=("PHP" "Ruby on Rails" "Node.js" "Go" "Python" "Rust")
-SELECTED_LANGUAGES="PHP","Ruby on Rails","Node.js"
+SELECTED_LANGUAGES="PHP","Ruby on Rails","Node.js","Go","Python"
 export OMAKMA_FIRST_RUN_LANGUAGES=$(gum choose "${AVAILABLE_LANGUAGES[@]}" --no-limit --selected "$SELECTED_LANGUAGES" --height 10 --header "Select programming languages")
