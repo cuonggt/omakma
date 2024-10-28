c2454fbeec5cd96c7281898172c3d19f3021932e
Cuong Giang <thaicuong.giang@gmail.com>
2024-10-28 08:33:22 +0700

    Add fastfetch config

--- install/terminal/app-fastfetch.sh
+++ install/terminal/app-fastfetch.sh
@@ -1,2 +1,9 @@
 # Display system information in the terminal
 brew install fastfetch
+
+# Only attempt to set configuration if fastfetch is not already set
+if [ ! -f "$HOME/.config/fastfetch/config.jsonc" ]; then
+  # Use Omakma fastfetch config
+  mkdir -p ~/.config/fastfetch
+  cp ~/.local/share/omakma/configs/fastfetch.jsonc ~/.config/fastfetch/config.jsonc
+fi
