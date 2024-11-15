052fdafd8222643f32520012a2786758cb855dd3
Cuong Giang <thaicuong.giang@gmail.com>
2024-11-15 08:18:04 +0700

    Remove LazyVim's .git folder

--- install/terminal/app-neovim.sh
+++ install/terminal/app-neovim.sh
@@ -7,6 +7,9 @@ if [ ! -d "$HOME/.config/nvim" ]; then
   # Use LazyVim
   git clone https://github.com/LazyVim/starter $HOME/.config/nvim
 
+  # Remove the .git folder, so you can add it to your own repo later
+	rm -rf ~/.config/nvim/.git
+
   # Make everything match the terminal transparency
   mkdir -p $HOME/.config/nvim/plugin/after
   cp $HOME/.local/share/omakma/configs/neovim/transparency.lua $HOME/.config/nvim/plugin/after/
