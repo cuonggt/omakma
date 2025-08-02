4d8c22b79fd56fb63c5864efcd086ab9b6e9c012
Cuong Giang <thaicuong.giang@gmail.com>
2025-08-03 00:16:05 +0700

    install Xcode Command Line Tools silently using softwareupdate --install

--- boot.sh
+++ boot.sh
@@ -1,27 +1,41 @@
+#!/bin/bash
+
 set -e
 
-ascii_art='                       __
-  ____   _____ _____  |  | _______    _____ _____
- /  _ \ /     \\__  \ |  |/ /\__  \  /     \\__  \
-(  <_> )  Y Y  \/ __ \|    <  / __ \|  Y Y  \/ __ \_
- \____/|__|_|  (____  /__|_ \(____  /__|_|  (____  /
-             \/     \/     \/     \/      \/     \/
-'
+ascii_art=' ██████╗ ███╗   ███╗ █████╗ ██╗  ██╗██╗███╗   ███╗ █████╗
+██╔═══██╗████╗ ████║██╔══██╗██║ ██╔╝██║████╗ ████║██╔══██╗
+██║   ██║██╔████╔██║███████║█████╔╝ ██║██╔████╔██║███████║
+██║   ██║██║╚██╔╝██║██╔══██║██╔═██╗ ██║██║╚██╔╝██║██╔══██║
+╚██████╔╝██║ ╚═╝ ██║██║  ██║██║  ██╗██║██║ ╚═╝ ██║██║  ██║
+ ╚═════╝ ╚═╝     ╚═╝╚═╝  ╚═╝╚═╝  ╚═╝╚═╝╚═╝     ╚═╝╚═╝  ╚═╝'
 
-echo -e "$ascii_art"
+clear
+echo -e "\n$ascii_art\n"
 echo "=> Omakma is for fresh macOS Sonoma 14.5 installations only!"
 echo -e "\nBegin installation (or abort with ctrl+c)..."
 
-sudo xcode-select --install
+if xcode-select -p &>/dev/null; then
+  echo -e "\nXcode Command Line Tools already installed."
+else
+  echo -e "\nInstalling Xcode Command Line Tools..."
+  LABEL=$(softwareupdate --list | grep -E '\*.*Command Line Tools' | head -1 | awk -F"*" '{print $2}' | sed -e 's/^ *//')
+  if [ -n "$LABEL" ]; then
+    sudo softwareupdate --install "$LABEL" --verbose
+    sudo xcode-select --switch /Library/Developer/CommandLineTools
+  else
+    echo -e "\nCould not find Command Line Tools in software update list."
+    exit 1
+  fi
+fi
 
-echo "Cloning Omakma..."
-rm -rf $HOME/.local/share/omakma
-git clone https://github.com/cuonggt/omakma.git $HOME/.local/share/omakma >/dev/null
+echo "\nCloning Omakma..."
+rm -rf ~/.local/share/omakma
+git clone https://github.com/cuonggt/omakma.git ~/.local/share/omakma >/dev/null
 if [[ $OMAKMA_REF != "master" ]]; then
-  cd $HOME/.local/share/omakma
-  git fetch origin "${OMAKMA_REF:-master}" && git checkout "${OMAKMA_REF:-master}"
+  cd ~/.local/share/omakma
+  git fetch origin "${OMAKMA_REF:-stable}" && git checkout "${OMAKMA_REF:-stable}"
   cd -
 fi
 
-echo "Installation starting..."
-source $HOME/.local/share/omakma/install.sh
+echo -e "\nInstallation starting..."
+source ~/.local/share/omakma/install.sh
