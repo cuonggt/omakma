73bb4df8a45ff02d329bdbef7437f51e7d84ca04
Cuong Giang <thaicuong.giang@gmail.com>
2025-08-07 11:58:30 +0700

    fix nord theme

--- themes/nord/sublime.sh
+++ themes/nord/sublime.sh
@@ -1,6 +1,6 @@
 SUBLIME_PACKAGE_NAME="Nord"
 SUBLIME_PACKAGE_GITHUB="nordtheme/sublime-text"
 SUBLIME_COLOR_SCHEME="Nord"
-SUBLIME_LIGHT_THEME="Nord"
-SUBLIME_DARK_THEME="Nord"
+SUBLIME_LIGHT_THEME="Default"
+SUBLIME_DARK_THEME="Default Dark"
 source $OMAKMA_PATH/themes/set-sublime-theme.sh
