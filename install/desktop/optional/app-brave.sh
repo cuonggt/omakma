dcfdf68c1e138476140c12624799dd76c27ff7f5
Cuong Giang <thaicuong.giang@gmail.com>
2026-02-25 23:39:52 +0700

    remove zed and brave

--- install/desktop/optional/app-brave.sh
+++
@@ -1,3 +0,0 @@
-if [[ ! -d /Applications/Brave\ Browser.app ]]; then
-  brew install --cask brave-browser
-fi
--- install/desktop/optional/app-zed.sh
+++
@@ -1,3 +0,0 @@
-if [[ ! -d /Applications/Zed.app ]]; then
-  brew install --cask zed
-fi
